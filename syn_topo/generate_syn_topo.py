897cd4086c8b7d2d86d51b1f7428237d3490cfb4
J. Barrett Carter <72092594+barrettcarter@users.noreply.github.com>
2021-07-06 16:41:44 -0400

    Updated function for syn topo generation
    This version is functional.

--- syn_topo/generate_syn_topo.py
+++ syn_topo/generate_syn_topo.py
@@ -8,48 +8,46 @@ This is a function that will be used to automatically generate synthetic topo-
 graphy from a specified elevation profile.
 
 The 'site_file_name' variable must be a csv file containing a column for distance
-labeled 'X_axis' and a column for elevation labeled 'Elevation'. The distance
-should be measured from a point on land to a point at sea, and the 'X_axis' values
-should be orded from largest to smallest.
+labeled 'Distance' and a column for elevation labeled 'Elevation'. The distance
+should be measured from a point on land to a point at sea, and the 'Distance' values
+should be orded from smallest to largest.
 
-The options for the shape parameter are either 'straight' or 'curved'. The options
+The options for the shape parameter are either 'straight', 'curved', or 'points'. The options
 for the bay parameter are True or False. The options for the plot parameter are
 0, 1, 2, or 3; 0 meaning no plot, 1 meaning just profile plot, 2 meaning just
 heatmap, 3 meaning both.
 
 """
-def generate_topo(site_file_name,shape,bay = False,plot = 3):
+def generate_topo(site_file_name,directory,shape,bay = False,plot = 3):
 
     import numpy as np
     from scipy.interpolate import CubicSpline
+    from scipy.interpolate import interp1d
     from matplotlib import pyplot as plt
     import seaborn as sns; sns.set_theme()
     
     #%%
     import pandas as pd
     import os
-    datadir = os.path.join('C:\\Users\\jbarrett.carter\\OneDrive\\CUAHSI-SI\\Topography') # directory for some sample data files
+    datadir = os.path.join(directory) # directory for elevation profile data files
     filename = site_file_name
     filepath = os.path.join(datadir, filename)
     df = pd.read_csv(filepath)
     
     #%%
     elev = df.loc[:,'Elevation']
-    dist = (df.loc[:,'X_axis']-1)*3
-    dist = (dist-max(dist))*(-1)
+    dist = df.loc[:,'Distance']
+    elev = elev[0:10001]
+    dist = dist[0:10001]
+    elev = elev.to_numpy()
+    elev = np.flip(elev)
+    dist = dist.to_numpy()
     #%%
     #Create spline model
     
     cs = CubicSpline(dist,elev)
     
     syn_elev = cs(dist)
-    
-    # %%
-    # Estimating the standard deviation for the noise
-    
-    # SD_sample = np.std(elev-syn_elev)
-    # noise = np.random.normal(0, .25, len(elev))
-    # syn_elev_rough = syn_elev + noise
     
     #%%
     
@@ -58,116 +56,143 @@ def generate_topo(site_file_name,shape,bay = False,plot = 3):
     if (plot == 1) or (plot == 3):
     
         plt.figure()
-        # plt.subplot(2, 1, 1)
         plt.plot(dist, elev, linewidth=2, label='Elevation Profile')
-        plt.plot(dist, syn_elev, linewidth=1, label='Syn topo smooth')
+        plt.plot(dist, syn_elev, linewidth=1, label='Spline Model')
         plt.xlabel('Distance (degrees)')
         plt.ylabel('Elevation (m)')
+        plt.title(site_file_name)
         plt.legend(loc='best')
-        
-        # plt.subplot(2, 1, 2)
-        # plt.plot(dist, elev, linewidth=2, label='Elev Prof Site 1')
-        # plt.plot(dist, syn_elev_rough, linewidth=1, label='Syn topo rough')
-        # plt.xlabel('Distance (degrees)')
-        # plt.ylabel('Elevation (m)')
-        # plt.legend(loc='best')
     
     #%%
     # Making it 3D
     
-    y = dist.to_numpy()
-    # x = np.linspace(0,5,num=len(y))
+    y = dist
     x = y
-    z = np.zeros((len(x),len(y)))
-    # xyz = np.zeros((1,3))
+    z = np.zeros((len(y),len(x)))
     
     #%%
     # Defining the shorline shoreline (z=0 contour)
-    below_sl = elev<0
-    below_sl = below_sl.to_numpy()
-    base_shore = int(max(np.argwhere(below_sl)))
+    above_sl = elev>-5
+    # above_sl = above_sl.to_numpy()
+    base_shore = int(min(np.argwhere(above_sl)))
     y_bs = y[base_shore]
     x_max = max(x)
     y_max = max(y)
+    
+    # give shoreline shape
+    if shape == 'straight':
+        s = np.ones(len(y))*y_bs # flat shoreline
     if shape == 'curved':
-        s = y_bs+5000*abs(np.sin(x*2*np.pi/x_max)) #curved with point
+        s = y_bs+2000*abs(np.sin((x)*np.pi/x_max)) # one big curve
+    if shape == 'points':
+        s = y_bs+2000*abs(np.sin((x+x_max/4)*2*np.pi/x_max)) #curved with points
+    
     # add in triangular bay centered at x = x_bay
     if bay == True:
-        s = y*0
+        sb = np.empty((len(y)))
+        sb[:]=np.NaN
         x_bay = x_max/2
-        bay_width = 1000
-        bay_height = 5000
+        bay_width = 4000
+        bay_height = 8000
         out_bay = np.logical_or(x<(x_bay-bay_width/2),x>(x_bay+bay_width/2))
         in_bay = out_bay==False
-        s[out_bay]= y_bs
-        s[in_bay] = y_bs+5000-abs(x[in_bay]-x_bay)*bay_height/(bay_width/2)
-        plt.plot(x,s)
-        plt.ylim([0,30000])
+        sb[out_bay]= np.NaN
+        sb[in_bay] = s[int(len(s)/2+bay_width/2/3)]+bay_height-abs(x[in_bay]-x_bay)*\
+            bay_height/(bay_width/2)
+        
+        # add in transition
+        
+        trans = np.empty((len(y)))
+        trans[:]=np.NaN
+        trans_width = 1000
+        out_trans = np.logical_or(x<(x_bay-bay_width/2-trans_width),x>(x_bay+bay_width/2+trans_width))
+        in_trans = out_trans==False
+        trans[out_trans]= np.NaN
+        trans[in_trans] = s[int(len(s)/2+(bay_width/2+trans_width)/3)]+bay_height-\
+            abs(x[in_trans]-x_bay)*bay_height/(bay_width/2)+trans_width*bay_height*2/bay_width
+        
+        # add in river
+        
+        river_width = bay_width/10
+        x_riv = np.logical_and(x > x_bay-river_width/2, x < x_bay+river_width/2)
     #%%
-    
-    col=1
+
     # to make 2d grid of z values
-    for col in range(z.shape[0]):
-        # z[:,col]=syn_elev*abs(np.sin(10*x[col]))
-        # z[:,col]=syn_elev*x[col]
-        # z[:,col]=syn_elev+5*(np.sin(x[col]*np.pi/x_max))
-        # z[:,col]=syn_elev
+    for col in range(z.shape[1]):
+
         ynew = y-s[col]+y_bs
         ynew[ynew<0]=0
         ynew[ynew>y_max]=y_max
         z[:,col]=cs(ynew)
-        # z[:,col]=cs(y)
-    
-    
-    # to make xyz matrix
-    # for col in range(len(y)):
-    #     zs=syn_elev*abs(np.sin(10*y[col]))
-    #     ys = np.ones((len(dist)))
-    #     ys = ys*y[col]
-    #     new_vals = np.stack((x,ys,zs),axis = 1)
-    #     topo = np.append(topo,new_vals,axis=0)
+        if (bay == True) and (in_bay[col]==True):
+            y_bay = np.logical_and(y>s[col],y<sb[col])
+            z[y_bay,col]=-5
+            if x_riv[col]==True:
+                y_riv = y>sb[col]
+                z[y_riv,col]=-5
     
     #%%
     
-    # ycross = y-s[10]+y_bs
-    # zcross = cs(ycross)
-    
-    # plt.plot(y,ycross)
-    # plt.plot(y,zcross)
-    #%%
-    # save topo to csv
-    # z_df = pd.DataFrame(z)
-    # z_df.to_csv(filepath,sep = '\t',index=False)
-    
-    
-    #%%
-    
-    #visualize cross sections
-    
-    # xind = 1000
-    
-    # plt.plot(y,z[:,xind])
+#Add in transition
+    if bay == True:
+        for yind in range(z.shape[0]):
+            if y[yind]>=min(trans[np.isnan(trans)==False]):
+                if y[yind]<max(sb[np.isnan(sb)==False]):
+                    
+                    zcross = z[yind,:]
+                    
+                    left_trans = np.logical_and(x<x_bay,trans>=y[yind])
+                    right_trans = np.logical_and(x>x_bay,trans>=y[yind])
+                    
+                    zcross[left_trans]=np.nan
+                    zcross[right_trans]=np.nan
+                    
+                    xsub = x[np.isnan(zcross)==False]
+                    zcross = zcross[np.isnan(zcross)==False]
+
+                    cs2 = interp1d(xsub,zcross)
+                    
+                    zcross_syn = cs2(x)
+                    
+                    z[yind,:]=zcross_syn
+                    
+                if y[yind]>=max(sb[np.isnan(sb)==False]):
+                    zcross = z[yind,:]
+                    
+                    left_trans = np.logical_and(x<x_bay,x>x_bay-river_width/2-trans_width)
+                    right_trans = np.logical_and(x>x_bay,x<x_bay+river_width/2+trans_width)
+                    
+                    zcross[left_trans]=np.nan
+                    zcross[right_trans]=np.nan
+                    
+                    xsub = x[np.isnan(zcross)==False]
+                    zcross = zcross[np.isnan(zcross)==False]
+                    
+                    cs2 = interp1d(xsub,zcross)
+                    
+                    zcross_syn = cs2(x)
+                    
+                    z[yind,:]=zcross_syn
     
     #%%
     
     #heatmap
     if (plot == 2) or (plot == 3):
-        xis = np.linspace(0,len(x),num = 100,endpoint=False).astype(int)
-        yis = np.linspace(0,len(y),num = 100,endpoint=False).astype(int)
-        zis = np.meshgrid(xis,yis,indexing = 'ij')
+        xis = np.linspace(0,len(x),num = 1000,endpoint=False).astype(int)
+        yis = np.linspace(0,len(y),num = 1000,endpoint=False).astype(int)
+        zis = np.meshgrid(yis,xis,indexing = 'ij')
         zsub = z[zis]
         zsub = np.flip(zsub)
-        # sns.color_palette("crest", as_cmap=True)
         plt.figure()
         ax = sns.heatmap(zsub,cmap='seismic',center=0)
     
-    #%%
-    # contour plot
-    # fig = plt.figure()
-    # ax = plt.axes(projection='3d')
-    # ax.contour3D(x,y,z)
     
     #%%
-    
-generate_topo(site_file_name='Shallotte_profile.csv',shape = 'curved', bay = False, plot = 3)
+
+my_dir = 'C:\\Users\\jbarrett.carter\\OneDrive\\CUAHSI-SI\\Topography\\Profiles'
+profiles = ['Atlantic_city_NJ.csv','Marley_beach_SC.csv','Melbourne_FL.csv',
+            'Savannah_GA.csv','Shallotte_NC.csv']
+generate_topo(site_file_name='Melbourne_FL.csv', directory = my_dir,shape = 'points', bay = True, plot = 3)
 # generate_topo(site_file_name='Shallotte_profile.csv',shape = 'curved', bay = True, plot = 3)
+for profile in profiles:
+    generate_topo(site_file_name=profile, directory = my_dir,shape = 'straight', bay = True, plot = 3)
