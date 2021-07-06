21383bd4a0cdaef5f0fa28f3ecf62281eb9f7923
J. Barrett Carter <72092594+barrettcarter@users.noreply.github.com>
2021-07-06 13:37:43 -0400

    Updated code for adding transition zone
    This portion of the code is still not finished.

--- syn_topo/Synthetic_topography_spline.py
+++ syn_topo/Synthetic_topography_spline.py
@@ -9,20 +9,28 @@ import numpy as np
 from scipy.interpolate import CubicSpline
 from matplotlib import pyplot as plt
 import seaborn as sns; sns.set_theme()
+from scipy.interpolate import interp2d
 
 #%%
 import pandas as pd
 import os
-datadir = os.path.join('C:\\Users\\jbarrett.carter\\OneDrive\\CUAHSI-SI\\Topography') # directory for some sample data files
+datadir = os.path.join('C:\\Users\\jbarrett.carter\\OneDrive\\CUAHSI-SI\\Topography\\Profiles') # directory for some sample data files
 #filename = 'Elevation_profile2.csv'
-filename = "Shallotte_profile.csv"
+filename = "Shallotte_NC.csv"
 filepath = os.path.join(datadir, filename)
 df = pd.read_csv(filepath)
 
 #%%
 elev = df.loc[:,'Elevation']
-dist = (df.loc[:,'X_axis']-1)*3
-dist = (dist-max(dist))*(-1)
+dist = df.loc[:,'Distance']
+elev = elev[0:10001]
+dist = dist[0:10001]
+elev = elev.to_numpy()
+elev = np.flip(elev)
+dist = dist.to_numpy()
+
+# dist = (df.loc[:,'X_axis']-1)*3
+# dist = (dist-max(dist))*(-1)
 #%%
 #Create spline model
 
@@ -43,9 +51,9 @@ syn_elev = cs(dist)
 
 plt.figure()
 # plt.subplot(2, 1, 1)
-plt.plot(dist, elev, linewidth=2, label='Elev Prof Site 1')
-plt.plot(dist, syn_elev, linewidth=1, label='Syn topo smooth')
-plt.xlabel('Distance (degrees)')
+plt.plot(dist/1000, elev, linewidth=2, label='Shallotte, NC Profile')
+plt.plot(dist/1000, syn_elev, linewidth=1, label='Spline model')
+plt.xlabel('Distance (km)')
 plt.ylabel('Elevation (m)')
 plt.legend(loc='best')
 
@@ -59,7 +67,7 @@ plt.legend(loc='best')
 #%%
 # Making it 3D
 
-y = dist.to_numpy()
+y = dist
 # x = np.linspace(0,5,num=len(y))
 x = y
 z = np.zeros((len(y),len(x)))
@@ -67,16 +75,19 @@ z = np.zeros((len(y),len(x)))
 
 #%%
 # Defining the shorline shoreline (z=0 contour)
-below_sl = elev<0
-below_sl = below_sl.to_numpy()
-base_shore = int(max(np.argwhere(below_sl)))
+# below_sl = elev<0
+# below_sl = below_sl.to_numpy()
+# base_shore = int(max(np.argwhere(below_sl)))
+above_sl = elev>-5
+# above_sl = above_sl.to_numpy()
+base_shore = int(min(np.argwhere(above_sl)))
 y_bs = y[base_shore]
 x_max = max(x)
 y_max = max(y)
 
 # s = np.ones(len(y))*y_bs # flat shore
-s = y_bs+2000*abs(np.sin((x)*np.pi/x_max)) # one big curve
-# s = y_bs+2000*abs(np.sin((x+x_max/4)*2*np.pi/x_max)) #curved with points
+# s = y_bs+2000*abs(np.sin((x)*np.pi/x_max)) # one big curve
+s = y_bs+2000*abs(np.sin((x+x_max/4)*2*np.pi/x_max)) #curved with points
 
 # add in triangular bay centered at x = x_bay
 sb = np.empty((len(y)))
@@ -130,9 +141,9 @@ for col in range(z.shape[1]):
         if x_riv[col]==True:
             y_riv = y>sb[col]
             z[y_riv,col]=-5
-    if in_trans[col]==True:
-        y_trans = np.logical_and(y>s[col],y<trans[col])
-        y_trans2 = np.logical_and(y>s[col],y<trans[col])
+    # if in_trans[col]==True:
+    #     y_trans = np.logical_and(y>s[col],y<trans[col])
+    #     y_trans2 = np.logical_and(y>s[col],y<trans[col])
     # z[:,col]=cs(y)
 
 
@@ -143,6 +154,38 @@ for col in range(z.shape[1]):
 #     ys = ys*y[col]
 #     new_vals = np.stack((x,ys,zs),axis = 1)
 #     topo = np.append(topo,new_vals,axis=0)
+#%%
+#Add in transition
+# xtrans = x[in_trans]
+# # ytrans = trans[in_trans]
+# ytrans = y[y>s[x==int(x_bay-bay_width-trans_width-1)]]
+# ztrans = z[x==xtrans,y==ytrans]
+
+yind = 8000
+zcross = z[yind,:]
+plt.plot(x,zcross)
+plt.title('y = 24 km -- before')
+
+left_trans = np.logical_and(x<x_bay,trans>=y[yind])
+right_trans = np.logical_and(x>x_bay,trans>=y[yind])
+
+zcross[left_trans]=np.nan
+zcross[right_trans]=np.nan
+
+xsub = x[np.isnan(zcross)==False]
+zcross = zcross[np.isnan(zcross)==False]
+
+plt.plot(xsub,zcross)
+plt.title('y = 24 km -- intermediate')
+
+cs2 = CubicSpline(xsub,zcross)
+
+zcross_syn = cs2(x)
+
+plt.plot(xsub,zcross,linewidth=2)
+plt.plot(x,zcross_syn)
+plt.title('y = 24 km -- after')
+
 
 #%%
 
@@ -161,24 +204,114 @@ for col in range(z.shape[1]):
 
 #visualize cross sections
 
-xind = 5000
+xind = 0
 
-plt.plot(y,z[:,xind])
+plt.plot(y[6000:7000],z[6000:7000,xind])
+plt.ylim((-10,10))
 
 #%%
 
 #heatmap
-xis = np.linspace(10000/3,20000/3,num = 100,endpoint=False).astype(int)
-yis = np.linspace(20000/3,30000/3,num = 100,endpoint=False).astype(int)
-# xis = np.linspace(0,len(x),num = 100,endpoint=False).astype(int)
-# yis = np.linspace(0,len(y),num = 100,endpoint=False).astype(int)
+# xis = np.linspace(10000/3,20000/3,num = 1000,endpoint=False).astype(int)
+# yis = np.linspace(20000/3,30000/3,num = 1000,endpoint=False).astype(int)
+xis = np.linspace(0,len(x),num = 1000,endpoint=False).astype(int)
+yis = np.linspace(0,len(y),num = 1000,endpoint=False).astype(int)
 zis = np.meshgrid(yis,xis,indexing = 'ij')
 zsub = z[zis]
 zsub = np.flip(zsub)
 # sns.color_palette("crest", as_cmap=True)
 ax = sns.heatmap(zsub,cmap='seismic',center=0)
 
+#%%
+#Add in transition (alternative)
+# xis = np.linspace(10000/3,20000/3,num = 100,endpoint=False).astype(int)
+# yis = np.linspace(20000/3,30000/3,num = 100,endpoint=False).astype(int)
+# xis = np.linspace(0,len(x),num = 100,endpoint=False).astype(int)
+# yis = np.linspace(0,len(y),num = 100,endpoint=False).astype(int)
+# xsub = x[xis]
+# ysub = y[yis]
+# zis = np.meshgrid(yis,xis,indexing = 'ij')
+# zsub = z[zis]
 
+xmin = x_bay-bay_width/2-trans_width
+xmax = x_bay+bay_width/2+trans_width
+ymin = min(s)
+ymax = max(y)
+
+# xsub = x[np.logical_and(x>xmin,x<xmax)]
+# ysub = y[np.logical_and(y>ymin,y<ymax)]
+# xis = np.argwhere(np.logical_and(x>xmin,x<xmax))
+# yis = np.argwhere(np.logical_and(y>ymin,y<ymax))
+
+xsub = np.linspace(xmin,xmax,num = 20)
+ysub = np.linspace(ymin,ymax,num = 20)
+xis = xsub/3
+xis = xis.astype(int)
+yis = ysub/3
+yis = yis.astype(int)
+zis = np.meshgrid(yis,xis,indexing = 'ij')
+zsub = z[zis]
+# zsub[zsub!=-5]=0
+# z[zis]=zsub # to reset
+
+# xis = np.linspace(0,len(x),num = 1000,endpoint=False).astype(int)
+# yis = np.linspace(0,len(y),num = 1000,endpoint=False).astype(int)
+# xsub = x[xis]
+# ysub = y[yis]
+# zis = np.meshgrid(yis,xis,indexing = 'ij')
+# zsub = z[zis]
+
+f = interp2d(xsub, ysub, zsub, kind='linear')
+# f = interp2d(x, y, z, kind='cubic')
+
+xsub = x[np.logical_and(x>xmin,x<xmax)]
+ysub = y[np.logical_and(y>ymin,y<ymax)]
+xis = np.argwhere(np.logical_and(x>xmin,x<xmax))
+yis = np.argwhere(np.logical_and(y>ymin,y<ymax))
+zis = np.meshgrid(yis,xis,indexing = 'ij')
+zsub = z[zis]
+z[zis]=zsub # to reset
+
+# znew = f(x,y)
+zsub_new = f(xsub,ysub)
+z[zis]=zsub_new
+
+#%%
+
+#heatmap
+xis = np.linspace(10000/3,20000/3,num = 1000,endpoint=False).astype(int)
+yis = np.linspace(20000/3,30000/3,num = 1000,endpoint=False).astype(int)
+# xis = np.linspace(0,len(x),num = 1000,endpoint=False).astype(int)
+# yis = np.linspace(0,len(y),num = 1000,endpoint=False).astype(int)
+zis = np.meshgrid(yis,xis,indexing = 'ij')
+zsub = z[zis]
+zsub = np.flip(zsub)
+# sns.color_palette("crest", as_cmap=True)
+ax = sns.heatmap(zsub,cmap='seismic',center=0)
+
+#%%
+
+#visualize cross sections
+
+xind = 750
+
+# plt.plot(y[6000:7000],z[6000:7000,xind])
+# plt.plot(y[6500:8000]/1000,z[6500:8000,xind],label = 'z')
+# plt.plot(y[6500:8000]/1000,znew[6500:8000,xind],label = 'znew')
+# plt.xlabel('Distance (km)')
+# plt.ylabel('Elevation (m)')
+# plt.legend()
+
+# plt.figure()
+# plt.plot(y,z[:,xind],label = 'z')
+# plt.plot(y,znew[:,xind],label = 'znew')
+# plt.legend()
+
+plt.figure()
+plt.plot(ysub,zsub[:,xind],label = 'z')
+plt.plot(ysub,zsub_new[:,xind],label = 'znew')
+plt.legend()
+# plt.ylim((-10,10))
 
 #%%
 # contour plot
@@ -193,3 +326,8 @@ b = np.array([4,5,6])
 c = np.array([7,8,9])
 d = a+b
 e=a*b
+#%%
+f = np.array(([1,2,3],[4,5,6],[7,8,9]))
+fsub = f[[1,1],[1,2]]
+fsub = [10,11]
+f[[1,1],[1,2]]=[20,30]
